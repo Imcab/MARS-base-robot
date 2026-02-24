b9531c92b23ecc5f2752faaacb8fb27e35542ba1
esangarr <esangarr@hotmail.com>
2026-02-24 10:07:40 -0600

    commit

--- src/main/java/frc/robot/configuration/constants/ModuleConstants/FlywheelConstants.java
+++ src/main/java/frc/robot/configuration/constants/ModuleConstants/FlywheelConstants.java
@@ -1,8 +1,11 @@
 package frc.robot.configuration.constants.ModuleConstants;
 
 public class FlywheelConstants {
-    public static final int kMotor1Port = 4;
-    public static final int kMotor2port = 5;
+    public static final int ShooterWheels1_ID = 4;
+    public static final int ShooterWheels2_ID = 5;
+
+    public static final int IntakeWheels_ID = 0;
+
     public static final double kGearing = 1;
     public static final double kMOI = 0.002;
     public static final double kRPMTolerance = 250;
