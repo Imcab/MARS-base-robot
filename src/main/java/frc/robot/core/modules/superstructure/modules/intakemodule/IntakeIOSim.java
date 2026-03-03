923638d1f9dd7e0b70cc67f1758d89a7dc6e0fef
Imcab <A01798415@tec.mx>
2026-03-03 10:46:23 -0600

    IntakeIOSim deleted mechanism2d

--- src/main/java/frc/robot/core/modules/superstructure/modules/intakemodule/IntakeIOSim.java
+++ src/main/java/frc/robot/core/modules/superstructure/modules/intakemodule/IntakeIOSim.java
@@ -7,13 +7,6 @@ import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
-import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
-import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
-import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj.util.Color8Bit;
-import frc.robot.configuration.KeyManager;
 
 import frc.robot.configuration.constants.ModuleConstants.IntakeConstants;
 import frc.robot.core.modules.superstructure.modules.intakemodule.IntakeIOKraken.intakeMODE;
@@ -25,12 +18,6 @@ public class IntakeIOSim implements IntakeIO{
     private double appliedVolts = 0.0;
     private boolean isClosedLoop = false;
     private double currentTargetAngle = 0.0;
-
-    private final Mechanism2d mech; 
-    private final MechanismRoot2d root;
-
-    private final MechanismLigament2d intake; 
-    private final MechanismLigament2d intakeTarget;
 
     public IntakeIOSim(){
 
@@ -53,23 +40,6 @@ public class IntakeIOSim implements IntakeIO{
             )
         );
 
-        this.mech = new Mechanism2d(1.0, 1.0);
-        this.root = mech.getRoot("Pivot", 0, 0.8);
-
-        this.intake = root.append(
-            new MechanismLigament2d(
-                "intake",
-                IntakeConstants.kIntakeLengthMeters,
-                0, 6, new Color8Bit(Color.kYellow))
-        );
-        
-        this.intakeTarget = root.append(
-            new MechanismLigament2d(
-                "intakeTarget",
-                IntakeConstants.kIntakeLengthMeters,
-                0, 3, new Color8Bit(Color.kRed))
-        );
-
     }
 
     @Override
@@ -87,14 +57,10 @@ public class IntakeIOSim implements IntakeIO{
 
         double simulatedDegrees = Units.radiansToDegrees(simIntake.getAngleRads());
 
-        intake.setAngle(simulatedDegrees);
-        intakeTarget.setAngle(currentTargetAngle);
-        
         inputs.position = simulatedDegrees;
         inputs.targetAngle = currentTargetAngle;
         inputs.appliedVolts = appliedVolts;
         
-        SmartDashboard.putData(KeyManager.INTAKE_KEY + "/Mech", mech);
     }
     
     @Override
