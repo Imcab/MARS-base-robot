b00c8846daeacaca2abc34e1c80f3972c16f55e1
Imcab <A01798415@tec.mx>
2026-03-15 12:06:54 -0600

    synx

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -4,7 +4,7 @@ package frc.robot;
  * DO NOT EDIT
  */
 public final class BuildConstants {
-    public static final String GIT_COMMIT = "44d8353";
-    public static final String GIT_BRANCH = "STV-4";
-    public static final String GIT_DATE = "2026-03-09 16:09:52 -0600";
+    public static final String GIT_COMMIT = "bf1dd93";
+    public static final String GIT_BRANCH = "main";
+    public static final String GIT_DATE = "2026-03-09 16:49:22 -0600";
 }
